2bb17f50b730c3e3a314ebd064e4659f1ddb27ce
Matias Andres Garcia Laure <garcialaurema@gmail.com>
2023-06-17 16:03:38 -0300

    Ejercicio 1 C#

---
+++ Ejercicio 1 recibir nombre e imprimirlo/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("El nombre del cliente es: " + nombre);
---
+++ Ejercicio 2 Obtener la hora/Program.cs
@@ -0,0 +1,3 @@
+﻿using System;
+string time = DateTime.Now.ToString();
+Console.WriteLine("La hora actual es: " + time);
